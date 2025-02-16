b93445862d69b1942add1daa887ff668e3899bff
Nguyễn Thị Khánh Linh <trankhanh9791@gmail.com>
2025-02-17 00:40:09 +0700

    Hello

---
+++ Hello/src/Hello.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+import java.util.Scanner;
+/**
+ * 
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Xin chao ban truoc man hinh nhe!");  
+	}
+
+}
