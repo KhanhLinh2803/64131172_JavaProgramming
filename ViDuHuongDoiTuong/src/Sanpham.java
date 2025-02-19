89d667d64d7b04fff2427191a7810ea22bf4f723
Nguyễn Thị Khánh Linh <trankhanh9791@gmail.com>
2025-02-19 17:50:45 +0700

    khoi tao ham tao
    ham tao Sanpham

--- ViDuHuongDoiTuong/src/Sanpham.java
+++ ViDuHuongDoiTuong/src/Sanpham.java
@@ -6,6 +6,16 @@ public class Sanpham {
 	String loaiSP;
 	String anhSP; // ten file anh
 	// ham tao
+	public Sanpham() { // cung ten, khong tham so
+		//khoi tao gia tri ban dau cho cac thuoc tinh
+	}
+	public Sanpham(int _maSP, String _tenSP, String _loaiSP, String _anhSP) { // cung ten, co tham so
+		//khoi tao gia tri ban dau cho cac thuoc tinh
+		maSP = _maSP;
+		tenSP= _tenSP;
+		loaiSP=_loaiSP;
+		anhSP=_anhSP;
+	}
 	// cac getter, setter
 
 }
